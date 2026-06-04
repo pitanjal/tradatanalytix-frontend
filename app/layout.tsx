18df51daf2f353a6e196eecbf0a387a4ac1988f6
pitanjal <109843171+tradatanalytix-web@users.noreply.github.com>
2026-06-04 09:21:24 +0530

    Remove extra script from layout analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,22 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  );
-}
-
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
